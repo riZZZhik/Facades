02d62b09ab2f5f3c8ded83b00fed9de85f3da939
riZZZhik <oldreddemon@gmail.com>
2020-03-01 02:09:36 +0300

    [ADD] Keras CPU or GPU training

--- facades.py
+++ facades.py
@@ -6,13 +6,23 @@ logging.getLogger("tensorflow").setLevel(logging.DEBUG)
 logging.basicConfig(filename='logs.log', filemode='w', level=logging.DEBUG,
                     format='%(asctime)s: %(levelname)s - %(message)s')
 
+# Setup model training device
+USE_CPU = False
+GPU_DEVICES = []
+
+if USE_CPU:
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+elif GPU_DEVICES:
+    os.environ["CUDA_VISIBLE_DEVICES"] = GPU_DEVICES
+
 import keras
 import numpy as np
-import tensorflow as tf
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import multi_gpu_model
 
 from ml import residual_model
 
@@ -32,8 +42,6 @@ class Facades:  # TODO: Predict  # TODO: Comments
         self.model = None
         self.train_generator, self.valid_generator = None, None
         self.batch_size = None
-
-        # TODO: Train init bool
         self.training_init_check = False
 
     # noinspection PyAttributeOutsideInit
@@ -75,8 +83,8 @@ class Facades:  # TODO: Predict  # TODO: Comments
         image_generator = data_datagen.flow_from_directory(dataset_dir + "train/data/", class_mode=None, seed=seed,
                                                            batch_size=batch_size, target_size=self.dim)
         masks_generator = masks_datagen.flow_from_directory(dataset_dir + "train/masks/", class_mode=None, seed=seed,
-                                                            color_mode="grayscale", batch_size=batch_size,
-                                                            target_size=self.dim)
+                                                            batch_size=batch_size, target_size=self.dim,
+                                                            color_mode="grayscale")
 
         self.train_generator = zip(image_generator, masks_generator)
 
@@ -98,40 +106,37 @@ class Facades:  # TODO: Predict  # TODO: Comments
         output_layer = residual_model(base_model, dropout_rate=0.5)
 
         self.model = Model(base_model.input, output_layer)
-        self.model.compile(optimizer=Adam(), loss="binary_crossentropy", metrics=["accuracy"])
         # keras.utils.plot_model(self.model, 'model.png', show_shapes=True)
 
-    def train(self, epochs=1, use_gpu=True):
+    def train(self, epochs=1, remote_monitor_ip="localhost:4959"):
         """Train model
         :param epochs: Number of epochs to train model
-        :param use_gpu: Whether to use GPU or CPU
+        :param remote_monitor_ip: IP where to run Keras RemoteMonitor (default: localhost:4959)  # TODO
         """
         # Check training init before train
         if not self.training_init_check:
             print('Please Initialize training variables before training (use "Facades.training_init" function)')
             return False
 
-        # Setup model training device
-        if use_gpu:
-            device = None  # TODO: Setup GPU training
-        else:
-            os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
-            os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-            device = '/cpu:0'
-
         # Initialize callbacks
         callbacks = [
-            EarlyStopping(patience=10, verbose=1),
-            ModelCheckpoint('checkpoints/weights.{epoch:02d}-{val_loss:.2f}.hdf5', verbose=1,
+            EarlyStopping(monitor='accuracy', patience=10, verbose=1),
+            ModelCheckpoint('checkpoints/weights.{epoch:02d}-{accuracy:.2f}.hdf5', verbose=1,
                             save_best_only=True, save_weights_only=True),
-            ReduceLROnPlateau(factor=0.1, patience=3, min_lr=0.00001, verbose=1)
+            ReduceLROnPlateau(monitor='accuracy', factor=0.2, patience=5, min_lr=0.001, verbose=1)
         ]
 
+        # Run on GPU if needed
+        if GPU_DEVICES:
+            self.model = multi_gpu_model(self.model, gpus=len(GPU_DEVICES))
+
         # Run model training
-        with tf.device(device):
-            self.model.fit_generator(self.train_generator, steps_per_epoch=np.floor(160 / self.batch_size),
-                                     epochs=epochs, callbacks=callbacks, verbose=2)
+        self.model.compile(optimizer=Adam(), loss="binary_crossentropy", metrics=["accuracy"])
+        self.model.fit_generator(self.train_generator, steps_per_epoch=np.floor(160 / self.batch_size),
+                                 epochs=epochs, callbacks=callbacks, verbose=2)
 
 
 if __name__ == "__main__":
-    facades = Facades((1024, 512))
+    facades = Facades((512, 512))
+    facades.training_init("dataset/", batch_size=5)
+    facades.train(10)
